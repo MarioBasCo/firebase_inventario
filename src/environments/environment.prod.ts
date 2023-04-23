be38a9817853582cc31dfcc9d90d3747713acd0f
MarioBasCo <mario-basilio1998@hotmail.com>
2023-04-23 12:32:15 -0500

    cambio base de datos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAx01m-yrz5u6DoPWy0MMTvUVWs5xupQjM",
-    authDomain: "inventario-7f05d.firebaseapp.com",
-    projectId: "inventario-7f05d",
-    storageBucket: "inventario-7f05d.appspot.com",
-    messagingSenderId: "545793910830",
-    appId: "1:545793910830:web:32a0e1309f0d6b1ce397bf"
+    apiKey: "AIzaSyCrBmyBEU5BYU7sFixhh5JeRjgIYmSxBjg",
+    authDomain: "app-ionic-inventario.firebaseapp.com",
+    projectId: "app-ionic-inventario",
+    storageBucket: "app-ionic-inventario.appspot.com",
+    messagingSenderId: "1047883611799",
+    appId: "1:1047883611799:web:7b60331a447efeb534b3d2"
   }
 };
